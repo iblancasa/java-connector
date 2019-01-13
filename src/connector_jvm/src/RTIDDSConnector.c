bb7bd93a43f52aa009da904436fa43f6bd95e272
Israel Blancas <iblancasa@gmail.com>
2019-01-14 00:38:42 +0100

    Add a way to test

--- src/connector_jvm/src/RTIDDSConnector.c
+++ src/connector_jvm/src/RTIDDSConnector.c
@@ -1,11 +1,11 @@
 #include "lua_binding/lua_binding_ddsConnector.h"
-#include "RTIDDSConnector.h"
+#include "rticonnector_RTIDDSConnector.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <jni.h>
 
 
-JNIEXPORT jlong JNICALL Java_RTIDDSConnector_RTIDDSConnector_1new
+JNIEXPORT jlong JNICALL Java_rticonnector_RTIDDSConnector_RTIDDSConnector_1new
   (JNIEnv *env, jobject obj){
 
       return (jlong) RTIDDSConnector_new(
